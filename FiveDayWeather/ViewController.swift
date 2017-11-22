742de45cbd33430d13644ab90645c030896db4ad
Vishnu Priya <vishnu.priya@futureplatforms.com>
2017-11-22 11:54:03 +0000

    Initial Commit

---
+++ FiveDayWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FiveDayWeather
+//
+//  Created by Vishnu Priya on 22/11/2017.
+//  Copyright © 2017 Vishnu Priya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
